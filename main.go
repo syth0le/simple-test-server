1cd90554dc56af17d3f23a10b447aa550994e510
syth0le <chdan565@gmail.com>
2022-04-17 13:39:20 +0300

    changed response for each served port

--- main.go
+++ main.go
@@ -23,17 +23,17 @@ func main() {
 	go http.ListenAndServe(":8081", mux)
 	mux2 := http.NewServeMux()
 	mux2.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "root %s", ":8081")
+		fmt.Fprintf(w, "root %s", ":8082")
 	})
 	go http.ListenAndServe(":8082", mux2)
 	mux3 := http.NewServeMux()
 	mux3.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "root %s", ":8081")
+		fmt.Fprintf(w, "root %s", ":8083")
 	})
 	go http.ListenAndServe(":8083", mux3)
 	mux4 := http.NewServeMux()
 	mux4.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "root %s", ":8081")
+		fmt.Fprintf(w, "root %s", ":8084")
 	})
 	go http.ListenAndServe(":8084", mux4)
 
