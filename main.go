c99609562dc07e41c2be516905bb07537e418866
syth0le <chdan565@gmail.com>
2022-04-17 13:58:29 +0300

    deleted useless parts

--- main.go
+++ main.go
@@ -5,17 +5,7 @@ import (
 	"net/http"
 )
 
-var ports = [4]string{"8081", "8082", "8083", "8084"}
-
 func main() {
-	//for _, port := range ports {
-	//	mux := http.NewServeMux()
-	//	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//		fmt.Fprintf(w, "root %s", port)
-	//	})
-	//	serverURI := fmt.Sprintf(":%s", port)
-	//	go http.ListenAndServe(serverURI, mux)
-	//}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "root %s", ":8081")
